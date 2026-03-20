b8e8fc9c03563d96a6825cc1fa7cdc25ea4a86a5
karthickdeveloper0071 <148254721+karthickdeveloper0071@users.noreply.github.com>
2026-03-20 13:18:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,5 +3,7 @@ import { PurchaseMasterPage } from './presentation/pages/PurchaseMasterPage.jsx'
 import './shared/styles/global.css';
 
 export default function App() {
+
+  
   return <PurchaseMasterPage />;
 }
